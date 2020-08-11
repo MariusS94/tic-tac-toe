4b291b4dbe5922434dbb5605042e8e7e287c0f4e
MariusS94 <marius1spomer@gmail.com>
2020-08-11 18:25:47 +0200

    Add nextPlayer winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,42 @@
 import "./board.css";
 import React from "react";
 import Square from "./Square";
+import { calculateWinner } from "../api/game";
 
 export default function Board() {
-  const status = "Next player: X";
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = React.useState(true);
+
+  const winner = calculateWinner(squares);
+
+  const nextPlayer = isXNext ? "X" : "O";
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = `Next player: ${nextPlayer}`;
+  }
+
+  function handleClick(index) {
+    const squaresClone = [...squares];
+    squaresClone[index] = nextPlayer;
+    setSquares(squaresClone);
+    setIsXNext(!isXNext);
+  }
 
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board">
-        <Square value="0" />
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-        <Square value="7" />
-        <Square value="8" />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
